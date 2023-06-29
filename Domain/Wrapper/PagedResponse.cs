df58c237b35d3ccbf3ea49880e0551aad202c607
Shahrom_developer <shahrom909050409@gmail.com>
2023-06-29 13:58:02 +0500

    test

--- Domain/Wrapper/PagedResponse.cs
+++ Domain/Wrapper/PagedResponse.cs
@@ -7,7 +7,7 @@ public class PagedResponse<T>:Response<T>
     public int PageNumber { get; set; }
     public int PageSize { get; set; }
     public int TotalPages { get; set; }
-    public int TotalRecords { get; set; }
+    public int TotalRecords { get; set; } 
 
     public PagedResponse(T data, int pageNumber, int pageSize,  int totalRecords) : base(data)
     {
